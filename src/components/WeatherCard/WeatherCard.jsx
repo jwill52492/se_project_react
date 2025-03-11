f3f25534d414db810667fb50148ca75a1707c08f
jwill52492 <jwill52492@gmail.com>
2025-03-11 16:38:01 -0400

    8

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -18,7 +18,7 @@ function WeatherCard({ weatherData }) {
   return (
     <section className="weather-card">
       <p className="weather-card__temperature">{weatherData?.temp?.F} &deg; F</p>
-      <img className="weather-card__image" src={weatherOption?.Url}
+      <img className="weather-card__image" src={weatherOption?.url}
       alt={`Card showing ${weatherData?.isday ? "day" : "night"}time ${weatherOption?.condition} weather`} />
     </section>
   );
