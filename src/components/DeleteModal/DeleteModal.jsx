934fd4691565f55cfc0660de5d77fee273e5f215
jwill52492 <jwill52492@gmail.com>
2025-04-01 16:40:17 -0400

    25

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -4,7 +4,7 @@ import "./DeleteModal.css";
 function DeleteModal ({ activeModal, onClose, item, onDeleteModalSubmit }) {
   function handleDeleteModalSubmit() {
     onDeleteModalSubmit(item);
-    removeItem(card);
+    removeItem(item);
   }
 
   return (
@@ -15,7 +15,7 @@ function DeleteModal ({ activeModal, onClose, item, onDeleteModalSubmit }) {
         </button>
         <p className="delete__text">Are you sure you want to delete this item? This action is irreversible</p>
         <button className="delete__confirm" type="button" onClick={handleDeleteModalSubmit} >Yes, delete item</button>
-        <button className="delete__cancel" type="button" onclick={onClose}>Cancel</button>
+        <button className="delete__cancel" type="button" onClick={onClose}>Cancel</button>
       </div>
     </div>
   )
